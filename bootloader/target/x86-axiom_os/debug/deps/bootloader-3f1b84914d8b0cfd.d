e7294124d7c7ed81c1a749527664e3bc5d5355c2
Sungbae Jeong <almagest0328@gmail.com>
2023-02-16 14:03:10 +0900

    remove target folder

--- bootloader/target/x86-axiom_os/debug/deps/bootloader-3f1b84914d8b0cfd.d
+++
@@ -1,8 +0,0 @@
-/Users/almagest/Github/AxiomOS/bootloader/target/x86-axiom_os/debug/deps/bootloader-3f1b84914d8b0cfd: src/main.rs src/./multiboot2_header.asm src/./vga.asm src/./boot.asm
-
-/Users/almagest/Github/AxiomOS/bootloader/target/x86-axiom_os/debug/deps/bootloader-3f1b84914d8b0cfd.d: src/main.rs src/./multiboot2_header.asm src/./vga.asm src/./boot.asm
-
-src/main.rs:
-src/./multiboot2_header.asm:
-src/./vga.asm:
-src/./boot.asm:
